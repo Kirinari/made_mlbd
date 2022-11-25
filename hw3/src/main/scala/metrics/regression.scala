ae18bae74c072cb6120d97e20bc055ad4a12cc0e
Kirinari <62498402+Kirinari@users.noreply.github.com>
2022-11-25 14:29:31 +0300

    Create regression.scala

---
+++ hw3/src/main/scala/metrics/regression.scala
@@ -0,0 +1,11 @@
+package metrics
+
+import breeze.linalg.{DenseVector, sum}
+
+package object regression  {
+    def MeanSquareError(yTrue: DenseVector[Double], yPred: DenseVector[Double]): Double = {
+        assert(yPred.size == yTrue.size, "size not equals {} {}".format(yTrue.size, yPred.size))
+
+        sum((yPred - yTrue).map(y => y * y)) * (1.0 / yTrue.size)
+    }
+}
